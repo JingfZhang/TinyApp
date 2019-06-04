efb8dc56155138d8d804e5f563d8fbb908d7d2d0
JingfZhang <zhangjingfan.1996@gmail.com>
2019-06-04 18:48:04 +0000

    Filling Out the urls_index.ejs Template

--- express_server.js
+++ express_server.js
@@ -21,6 +21,11 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  let templetVars = { urls: urlDatabase};
+  res.render("urls_index", templetVars);
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
