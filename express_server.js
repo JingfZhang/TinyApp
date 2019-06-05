cdcf2d5c6b29a925e35ee23bde8e7b10064edf19
JingfZhang <zhangjingfan.1996@gmail.com>
2019-06-05 17:22:22 +0000

    Added edit button

--- express_server.js
+++ express_server.js
@@ -59,6 +59,12 @@ app.post("/urls/:shortURL/delete", (req,res) => {
   res.redirect("/urls");
 })
 
+app.post("/urls/:shortURL/update", (req,res) => {
+  urlDatabase[req.params.shortURL] = req.body.newURL;
+  res.redirect("/urls");
+
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
