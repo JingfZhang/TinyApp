0d83be67f43f5564f9e3799bda1f3e2f2eaea3e5
JingfZhang <zhangjingfan.1996@gmail.com>
2019-06-06 21:00:14 +0000

    encrypted cookies

--- express_server.js
+++ express_server.js
@@ -1,12 +1,18 @@
+const cookieSession = require("cookie-session");
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const cookieParser = require('cookie-parser');
 
-const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ["key1", "key2"],
+
+  maxAge: 24 * 60 * 60 * 1000
+}))
 
 app.set("view engine", "ejs");
 
@@ -39,17 +45,18 @@ const urlDatabase = {
   "b2xVn2": {longURL: "http://www.lighthouselabs.ca", userID: "aJ48lW"},
   "9sm5xK": {longURL: "http://www.google.com", userID: "aJ48lW"}
 };
-
 const users = {
   "aJ48lW": {
     id: "aJ48lW",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur"
+    //purple-monkey-dinosaur
+    password: "$2b$10$CKO/ZjvMkJCV7GcnZdz9MuhlEAD9qQnR4QBqvEis7ro56Gb0aEBPG"
   },
  "user2RandomID": {
     id: "user2RandomID",
     email: "user2@example.com",
-    password: "dishwasher-funk"
+    //dishwasher-funk
+    password: "$2b$10$Aw3Keor3AzfUq7WeSDhxQOP2xKvLhTUFXpJR4BkYYDDAI.k2cSdMC"
   }
 }
 
@@ -66,13 +73,13 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templetVars = { urls: urlDatabase, userId: req.cookies["user_id"], shortURLs: urlsForUser(req.cookies["user_id"]), user: users[req.cookies["user_id"]]};
+  let templetVars = { urls: urlDatabase, userId: req.session.user_id, shortURLs: urlsForUser(req.session.user_id), user: users[req.session.user_id]};
   res.render("urls_index", templetVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  if (req.cookies["user_id"]) {
-    let templetVars = {user: users[req.cookies["user_id"]]};
+  if (req.session.user_id) {
+    let templetVars = {user: users[req.session.user_id]};
     res.render("urls_new", templetVars);
   } else {
     res.redirect("/login");
@@ -80,13 +87,13 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]["longURL"], user: users[req.cookies["user_id"]] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]["longURL"], user: users[req.session.user_id] };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
   let newId = generateRandomString()
-  urlDatabase[newId] = {longURL: req.body.longURL, userID: req.cookies["user_id"]};
+  urlDatabase[newId] = {longURL: req.body.longURL, userID: req.session.user_id};
   res.redirect("/urls");
 });
 
@@ -96,29 +103,32 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.post("/urls/:shortURL/delete", (req,res) => {
-  if (req.cookies["user_id"] === urlDatabase[req.params.shortURL]["userID"]) {
+  if (req.session.user_id === urlDatabase[req.params.shortURL]["userID"]) {
     delete urlDatabase[req.params.shortURL];
   }
   res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/update", (req,res) => {
-  if (req.cookies["user_id"] === urlDatabase[req.params.shortURL]["userID"]) {
-    urlDatabase[req.params.shortURL] = req.body.newURL;
+  if (req.session.user_id === urlDatabase[req.params.shortURL]["userID"]) {
+    urlDatabase[req.params.shortURL] = {
+      longURL: req.body.newURL,
+      userID: req.session.user_id
+    }
   }
   res.redirect("/urls");
 });
 
 app.get("/login", (req, res) => {
-  let templetVars = { user: users[req.cookies["user_id"]]};
+  let templetVars = { user: users[req.session.user_id]};
   res.render("urls_login", templetVars);
 })
 
 app.post("/login", (req, res) => {
   let user = isRegistered(req.body.email);
   if (user) {
-    if (req.body.password === user["password"]) {
-      res.cookie("user_id", user["id"]);
+    if (bcrypt.compareSync(req.body.password, user["password"])) {
+      req.session.user_id = user["id"];
       res.redirect("/urls");
     } else {
       res.statusCode = 403;
@@ -131,12 +141,12 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id");
+  req.session = null
   res.redirect("/urls");
 });
 
 app.get("/register", (req, res) => {
-  let templetVars = { user: users[req.cookies["user_id"]]};
+  let templetVars = { user: users[req.session.user_id]};
   res.render("urls_registration", templetVars);
 });
 
@@ -152,9 +162,9 @@ app.post("/register", (req, res) => {
     users[userId] = {
       id: userId,
       email: req.body.email,
-      password: req.body.password
+      password: bcrypt.hashSync(req.body.password, 10)
     };
-    res.cookie("user_id", userId);
+    req.session.user_id = userId;
     res.redirect("/urls");
   }
 });
